ef70e6178f2883ebea702493f1d60fb1439332e8
Leo <leonardodeaguiar@outlook.com.br>
2023-08-23 13:05:40 -0300

    feat: implemented error handling

--- src/drivers/http_requester.py
+++ src/drivers/http_requester.py
@@ -1,11 +1,12 @@
 from typing import Dict
 import requests
+from .interfaces.http_requester import HttpRequesterInterface
 
 
-class HttpRequester:
+class HttpRequester(HttpRequesterInterface):
     def __init__(self) -> None:
         self.__url = "https://web.archive.org/web/20120916140934/http://www.nga.gov/collection/anZ1.htm"
 
     def request_from_page(self) -> Dict[int, str]:
-        response = requests.get(self.__url, timeout=200)
+        response = requests.get(self.__url, timeout=20000)
         return {"status_code": response.status_code, "html": response.text}  # type: ignore
---
+++ src/stages/extract/extract_html_test.py
@@ -0,0 +1,30 @@
+from src.drivers.http_requester import HttpRequester
+from src.drivers.html_collector import HtmlCollector
+from src.stages.contracts.extract_contract import ExtractContract
+from src.errors.extract_error import ExtractError
+from .extract_html import ExtractHtml
+
+
+def test_extract():
+    http_requester = HttpRequester()
+    http_collector = HtmlCollector()
+
+    extract_html = ExtractHtml(http_requester, http_collector)
+    response = extract_html.extract()
+    print()
+    # print(response)
+
+    assert isinstance(response, ExtractContract)
+
+
+def test_extract_error():
+    http_requester = "Vai dar Errado"
+    http_collector = HtmlCollector()
+
+    extract_html = ExtractHtml(http_requester, http_collector)
+
+    try:
+        extract_html.extract()
+    except Exception as exception:
+        assert isinstance(exception, ExtractError)
+        print(exception)
